d5fba1c514db11a04b8e81a8ca2af18060e9afdb
Aline Cegatto <aline.guttner@gmail.com>
2022-12-13 18:28:53 -0300

    commit -m "add imagens no carousel com js pt2"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fotosShows = [
+let fotosShows = [
     {
         "src": "imagens/halloween1.jpg",
         "local": "A TOCA - 22/10/2022 - ITATIBA - SP"
@@ -17,7 +17,49 @@ const fotosShows = [
     }
 ]
 
+let localArr = []
+
 function carregarImagensShows() {
+    fotosShows.forEach(foto => {
+        if (localArr.includes(foto.local)) {
+            return
+        } else {
+            localArr.push(foto.local)
+            let fotosMesmoDia = fotosShows.filter(fotoShow => fotoShow.local === foto.local)
+            console.log(fotosMesmoDia)
+
+            let local = document.createElement('p')
+            local.innerHTML = foto.local
+
+            let imgContainer = document.createElement('div')
+            imgContainer.classList.add('img-container')
+
+            fotosMesmoDia.forEach(photo => {
+                let imagem = document.createElement('img')
+                imagem.setAttribute('src', photo.src)
+                imgContainer.appendChild(imagem)
+            })
+
+            let itemCarousel = document.createElement('div')
+            itemCarousel.classList.add('item-carrosel')
+
+            itemCarousel.append(imgContainer, local)
+
+            let carouselItem = document.createElement('div')
+            carouselItem.classList.add('carousel-item')
+            carouselItem.setAttribute('data-bs-interval', "999999999")
+
+            carouselItem.appendChild(itemCarousel)
+
+            $('#carouselInnerShows').append(carouselItem)
+            $('#carouselInnerShows div:first').addClass('active')
+        }
+    })
+}
+
+carregarImagensShows()
+
+function carregarImagensShows2() {
 
     fotosShows.forEach(foto => {
         let imagem = document.createElement('img')
@@ -37,8 +79,8 @@ function carregarImagensShows() {
 
         carouselItem.appendChild(itemCarousel)
 
-        $('#carouselInnerShows').append(carouselItem)
-        $('#carouselInnerShows div:first').addClass('active')
+        $('#carouselInnerShows2').append(carouselItem)
+        $('#carouselInnerShows2 div:first').addClass('active')
     })
 }
-carregarImagensShows()
+carregarImagensShows2()
